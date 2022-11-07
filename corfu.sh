7d6d751e230cddd9b6f2881046a2d4a1a9c5409d
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2022-11-07 16:45:24 -0500

    update script to push docker image

--- corfu.sh
+++ corfu.sh
@@ -1,4 +1,4 @@
 docker pull atwalter/acl2s_gradescope_autograder:cs2800fa22 
-docker build -t hwk05 .
-docker tag hwk05 ankitku88/hwk05
-docker push ankitku88/hwk05
+docker build -t dfa_test .
+docker tag dfa_test ankitku88/dfa_test
+docker push ankitku88/dfa_test
