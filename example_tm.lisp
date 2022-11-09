5e62139c3d20830325afee92cd8a7765535c2add
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2022-11-09 07:51:00 -0500

    changed instructor's TM

--- example_tm.lisp
+++ example_tm.lisp
@@ -12,13 +12,13 @@
 (gen-tm
  :name instructor-tm
  :states (q0 q1 q2 q3)
- :alphabet (#\0 #\1)
- :tape-alphabet (#\0 #\1 nil)
+ :alphabet (0 1)
+ :tape-alphabet (0 1 nil)
  :start-state q0
  :accept-state q1
  :reject-state q2
- :transition-fun (((q0 #\1) . (q0 #\0 R))
-                  ((q0 #\0) . (q0 #\1 R))
+ :transition-fun (((q0 1) . (q0 0 R))
+                  ((q0 0) . (q0 1 R))
                   ((q0 nil) . (q3 nil R))
                   ((q3 nil) . (q1 nil L))))
 
